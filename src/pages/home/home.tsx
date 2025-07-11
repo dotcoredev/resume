72d9f68f2fea1abfd3e236132e88297701d88562
devshanematte <devshanematte@gmail.com>
2025-07-11 14:14:11 +0500

    change index.html

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,7 +1,7 @@
 export const HomePage = () => {
 	return (
 		<main>
-			<h1>Welcome to the Home Page Nifeev.ru!!!!</h1>
+			<h1>Welcome to the Home Page DotCore.ru!!!!</h1>
 			<p>This is the home page of the application.</p>
 		</main>
 	);
