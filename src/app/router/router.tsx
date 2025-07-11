a7b52762d110e935010df13b38652bf9fe3f4ecc
devshanematte <devshanematte@gmail.com>
2025-07-11 10:45:31 +0500

    update files and vite

--- src/app/router/router.tsx
+++ src/app/router/router.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import MainLayout from "../layouts/main";
 import ResumePage from "../../pages/resume";
+import HomePage from "../../pages/home";
 
 export const RouterProvider = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
+				<Route path="/" element={<MainLayout />}>
+					<Route index element={<HomePage />} />
+				</Route>
 				<Route path="/resume" element={<MainLayout />}>
 					<Route index element={<ResumePage />} />
 				</Route>
