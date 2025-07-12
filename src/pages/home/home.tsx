04987c03f2c48088b7d290a52f6e1f3c7ded863e
devshanematte <devshanematte@gmail.com>
2025-07-12 09:33:03 +0500

    update

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,7 +1,8 @@
 export const HomePage = () => {
 	return (
 		<main>
-			<h1>Welcome to the Home Page DotCore.ru!!!!</h1>
+			<h1>Welcome to the Home Page DotCore.ru!</h1>
+			<p>-----------------------</p>
 			<p>This is the home page of the application.</p>
 		</main>
 	);
