fee4772df2b9df3ca6539c505ada14fdf9d77370
devshanematte <devshanematte@gmail.com>
2025-07-13 21:38:09 +0500

    update

--- src/pages/design-template/api/patterns/patterns.api.ts
+++ src/pages/design-template/api/patterns/patterns.api.ts
@@ -1,6 +1,6 @@
 export async function getPatternsMD() {
 	return new Promise((resolve) => {
-		fetch("/src/shared/assets/HOW_TO_REMEMBER_PATTERNS.md")
+		fetch("/HOW_TO_REMEMBER_PATTERNS.md")
 			.then((response) => response.text())
 			.then((text) => resolve(text));
 	});
